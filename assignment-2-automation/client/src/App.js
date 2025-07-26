6d4dbe9b5eaf7ac19a73522c547b647d56642f28
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-07-26 18:32:04 +0530

    Insert user data in the data in admin dasbaord

--- assignment-2-automation/client/src/App.js
+++ assignment-2-automation/client/src/App.js
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://gradnext-assignment.onrender.com/api/submissions")
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.error("Failed to fetch:", err));
+  }, []);
+
+  // console.log(users);
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-      <div className="overflow-auto bg-white shadow rounded-lg">
-        <table className="min-w-full text-sm text-left">
-          <thead className="bg-gray-200">
+      <h1 className="text-3xl font-semibold mb-6 text-gray-800">
+        Admin Dashboard
+      </h1>
+      <div className="overflow-x-auto bg-white shadow-md rounded-xl">
+        <table className="min-w-full text-sm text-left text-gray-700">
+          <thead className="bg-gray-300 text-gray-700 uppercase text-xs font-semibold">
             <tr>
               <th className="p-3">Name</th>
               <th className="p-3">Email</th>
@@ -20,7 +31,22 @@ function App() {
               <th className="p-3">Submitted</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {users &&
+              users.map((user) => (
+                <tr key={user._id} className="border-b">
+                  <td className="p-3">{user.name}</td>
+                  <td className="p-3">{user.email}</td>
+                  <td className="p-3">{user.phone}</td>
+                  <td className="p-3">{user.emailOpened ? "✅" : "❌"}</td>
+                  <td className="p-3">{user.clickedLink ? "✅" : "❌"}</td>
+                  <td className="p-3">{user.paymentComplete ? "✅" : "❌"}</td>
+                  <td className="p-3">
+                    {new Date(user.createdAt).toLocaleDateString()}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
         </table>
       </div>
     </div>
